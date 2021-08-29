d51f8ca96de35cd7b61fb34da5f0fb19bfbe272e
Mario <mj.empresas.contacto@gmail.com>
2021-08-29 17:40:12 -0500

    Version 1.0.8

---
+++ js/nav.js
@@ -0,0 +1,29 @@
+// const fijo = document.querySelector('nav');
+// // const texto = document.querySelector('#links i');
+// const selector = document.querySelector('.links');
+
+// window.addEventListener('scroll', () => {
+//     const scrollpx = window.scrollY;
+
+//     if( scrollpx >= 1 ) {
+        
+//         //Clase en el header
+//         fijo.classList.contains('bg-primary', 'navbar-dark');
+//         fijo.classList.add('bg-primary', 'navbar-dark');
+//     }else if( scrollpx <= 1 ){
+       
+//         //Clase en el header
+//         fijo.classList.contains('bg-primary', 'navbar-dark');
+//         fijo.classList.remove('bg-primary', 'navbar-dark');
+//     }
+// });
+
+// // console.log(texto)
+
+
+
+// window.addEventListener('scroll', cambiarClase);
+
+// function cambiarClase() {
+//     const scrol = window.scrollY;
+// }
